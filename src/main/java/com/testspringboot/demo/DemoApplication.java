0d398b8032bab11541d57ed15003ffd2111bd326
AngelEvaristoAledo <aevaristoa2@hotmail.com>
2026-03-08 19:15:56 -0500

    Change weather message to Spanish

--- src/main/java/com/testspringboot/demo/DemoApplication.java
+++ src/main/java/com/testspringboot/demo/DemoApplication.java
@@ -11,7 +11,7 @@ public class DemoApplication {
 
 	@GetMapping("/weatherForecast")
 	public String getMessage() {
-		return "Today is a great sunny day for";
+		return "Hoy es un dia claro para promagar con Java";
 	}
 
 	public static void main(String[] args) {
